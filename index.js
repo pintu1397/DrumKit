ea396cb06edd862cad37b143bea1f8de3d8b725e
Pintu Kumar <pintupn1397@gmail.com>
2022-07-14 10:04:59 +0530

    drum kit

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+//detecting button press.
+var n=document.querySelectorAll(".drum").length;
+for(var i=0;i<n;i++){
+
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+   
+    //this.style.color="white";
+    var buttoninnerHTML=this.innerHTML;
+   
+     makesound(buttoninnerHTML);
+     buttonAnimation(buttoninnerHTML);
+
+});
+
+}
+
+//detecting keyboard press
+document.addEventListener("keypress",function(event) {
+
+    makesound(event.key);
+    buttonAnimation(event,key);
+});
+function makesound(key){
+
+    switch (key) {
+        case "w":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+            
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+               tom3.play();
+               break;
+
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+               tom4.play();
+            break;
+
+        case "j":
+            var snare=new Audio("sounds/tom-4.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            var crash=new Audio("sounds/tom-4.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            var kickbase=new Audio("sounds/tom-4.mp3");
+            kickbase.play();
+            break;
+                        
+        default:
+            console.log(buttoninnerHTML);
+            break;
+    }
+
+}
+
+function  buttonAnimation(currentKey){
+
+   var activeButton=document.querySelector("."+currentKey);
+     activeButton.classList.add("pressed");
+
+     setTimeout(function(){
+        activeButton.classList.remove("pressed");
+     },100);
+}
+//var audio=new Audio("sounds/tom-1.mp3");
+//audio.play();
